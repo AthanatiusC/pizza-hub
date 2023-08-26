d5bfb7c0c23d74e40b88578e230bd5f73f5b4842
Lexi Anugrah <lexi_anugrah@hotmail.com>
2023-08-27 06:46:08 +0700

    add more test case

--- service/service_test.go
+++ service/service_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"testing"
 
+	"github.com/AthanatiusC/pizza-hub/model"
 	"github.com/AthanatiusC/pizza-hub/repository"
 )
 
@@ -18,5 +19,57 @@ func TestGetMenus(t *testing.T) {
 
 	if response == nil {
 		t.Errorf("Expected not nil, got nil")
+	}
+}
+
+func TestAddChef(t *testing.T) {
+	repository := repository.NewRepository()
+	service := NewService(repository)
+
+	response, err := service.AddChef(context.TODO(), model.Chef{
+		Name: "Test Chef",
+	})
+	if err != nil {
+		t.Errorf("Error: %v", err)
+	}
+
+	if response.ID == "" {
+		t.Errorf("Expected id not nil, got nil")
+	}
+}
+
+func TestAddOrder(t *testing.T) {
+	repository := repository.NewRepository()
+	service := NewService(repository)
+
+	response, err := service.AddOrder(context.TODO(), model.OrderRequest{
+		MenuID: 1,
+	})
+	if err != nil {
+		t.Errorf("Error: %v", err)
+	}
+
+	if response.ID == "" {
+		t.Errorf("Expected id not nil, got nil")
+	}
+}
+
+func TestGetOrder(t *testing.T) {
+	repository := repository.NewRepository()
+	service := NewService(repository)
+
+	order, err := service.AddOrder(context.TODO(), model.OrderRequest{
+		MenuID: 1,
+	})
+	if err != nil {
+		t.Errorf("Error: %v", err)
+	}
+
+	response, err := service.GetOrder(context.TODO(), order.ID)
+	if err != nil {
+		t.Errorf("Error: %v", err)
+	}
+	if response.ID != order.ID {
+		t.Errorf("Expected id %s, got %s", order.ID, response.ID)
 	}
 }
